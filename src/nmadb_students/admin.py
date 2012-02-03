be4390c8a91f17a5efb8d2e82160f1c8781b96c9
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-02-03 16:19:33 +0200

    Improved admin page.

--- src/nmadb_students/admin.py
+++ src/nmadb_students/admin.py
@@ -29,10 +29,79 @@ class SchoolAdmin(admin.ModelAdmin):
             )
 
 
+class StudyRelationInline(admin.TabularInline):
+    """ Inline for school information.
+    """
+
+    model = models.StudyRelation
+    extra = 0
+
+
+class ParentRelationInline(admin.TabularInline):
+    """ Inline for parent information.
+    """
+
+    model = models.ParentRelation
+    extra = 0
+
+
+class AlumniInline(admin.StackedInline):
+    """ Inline administration for information about alumni.
+    """
+
+    model = models.Alumni
+    extra = 0
+
+
+class SocialDisadvantageMarkInline(admin.TabularInline):
+    """ Inline administration for social disadvantage mark.
+    """
+
+    model = models.SocialDisadvantageMark
+    extra = 0
+
+
+class DisabilityMarkInline(admin.TabularInline):
+    """ Inline administration for disability mark.
+    """
+
+    model = models.DisabilityMark
+    extra = 0
+
+
 class StudentAdmin(contacts_admin.HumanAdmin):
     """ Administration for student.
     """
 
+    inlines = contacts_admin.HumanAdmin.inlines + [
+            StudyRelationInline,
+            #ParentRelationInline,   FIXME: Why doesn't work?
+            AlumniInline,
+            SocialDisadvantageMarkInline,
+            DisabilityMarkInline,
+            ]
+
+
+class ParentRelationAdmin(admin.ModelAdmin):
+    """ Administration for parent relation.
+    """
+
+    list_display = [
+            'child',
+            'parent',
+            'relation_type',
+            ]
+
+    search_fields = [
+            'child__first_name',
+            'child__last_name',
+            'child__old_last_name',
+            'parent__first_name',
+            'parent__last_name',
+            'parent__old_last_name',
+            ]
+
 
 admin.site.register(models.School, SchoolAdmin)
 admin.site.register(models.Student, StudentAdmin)
+admin.site.register(models.ParentRelation, ParentRelationAdmin)
